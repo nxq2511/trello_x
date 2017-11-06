40ee2ad9acffdba3764924cecd1b186a86fbb750
Quynh Nguyen <quynh.nguyenxuan@powergatesoftware.com>
2017-11-06 17:35:54 +0700

    set DB

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'trello_x'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'root'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
